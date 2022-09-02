8e59d4235724ff9bed56918f568ecec6cb2ac4a8
NyanPham <phamthanhnhanussh@gmail.com>
2022-09-02 19:38:15 +0700

    Added variants json data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -5,6 +5,7 @@ const Product = require('../../models/productModel')
 const User = require('../../models/userModel')
 const Collection = require('../../models/collectionModel')
 const Category = require('../../models/categoryModel')
+const Variant = require('../../models/variantModel')
 
 dotenv.config({ path: `${__dirname}/../../config.env` })
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
@@ -13,13 +14,15 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'))
 const collections = JSON.parse(fs.readFileSync(`${__dirname}/collections.json`, 'utf-8'))
 const categories = JSON.parse(fs.readFileSync(`${__dirname}/categories.json`, 'utf-8'))
+const variants = JSON.parse(fs.readFileSync(`${__dirname}/variants.json`, 'utf-8'))
 
 const importData = async () => {
     try {
-        await Product.create(products)
-        await User.create(users)
+        // await Product.create(products)
+        // await User.create(users)
         // await Collection.create(collections)
         // await Category.create(categories)
+        await Variant.create(variants)
 
         console.log('Database is imported successfully')
     } catch (err) {
