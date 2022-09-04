748d8137a747b707f3263a1c327150d88a0880b3
NyanPham <phamthanhnhanussh@gmail.com>
2022-09-04 17:08:48 +0700

    m

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down server...')
+    console.log(err.message)
+    server.close(() => {
+        process.exit(1)
+    })
+})
+
+dotenv.config({ path: `./config.env` })
 const app = require('./app')
 
-dotenv.config({ path: `${__dirname}/config.env` })
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
 mongoose.connect(DB).then(() => console.log('Database is successfully connected!'))
@@ -19,12 +27,4 @@ process.on('unhandledRejection', (err) => {
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
     console.log(`App is running at port ${port}`)
-})
-
-process.on('uncaughtException', (err) => {
-    console.log('UNCAUGHT EXCEPTION! Shutting down server...')
-    console.log(err.message)
-    server.close(() => {
-        process.exit(1)
-    })
 })
