577361a24347487c243619013c0d7996ff9474eb
NyanPham <phamthanhnhanussh@gmail.com>
2022-09-04 19:04:25 +0700

    node

--- controllers/biddingController.js
+++ controllers/biddingController.js
@@ -1,26 +1,26 @@
 const factory = require('./factoryHandler')
-// const Bidding = require('../models/biddingModel')
+const Bidding = require('../models/auctionModel')
 
-// exports.getProductAndUserIds = (req, res, next) => {
-//     if (req.params.productId) req.body.product = req.params.productId
-//     if (req.params.userId) req.body.user = req.params.userId
+exports.getProductAndUserIds = (req, res, next) => {
+    if (req.params.productId) req.body.product = req.params.productId
+    if (req.params.userId) req.body.user = req.params.userId
 
-//     const parsedUrl = req.originalUrl.split('/')
+    const parsedUrl = req.originalUrl.split('/')
 
-//     if (parsedUrl[parsedUrl.length - 1].startsWith('my')) {
-//         req.body.user = req.user._id
-//     }
+    if (parsedUrl[parsedUrl.length - 1].startsWith('my')) {
+        req.body.user = req.user._id
+    }
 
-//     next()
-// }
+    next()
+}
 
-// exports.getUserToBid = (req, res, next) => {
-//     if (req.user) req.body.user = req.user._id
-//     next()
-// }
+exports.getUserToBid = (req, res, next) => {
+    if (req.user) req.body.user = req.user._id
+    next()
+}
 
-// exports.getAllBiddings = factory.getAll(Bidding)
-// exports.createBidding = factory.createOne(Bidding)
-// exports.getBidding = factory.getOne(Bidding)
-// exports.updateBidding = factory.updateOne(Bidding)
-// exports.deleteBidding = factory.deleteOne(Bidding)
+exports.getAllBiddings = factory.getAll(Bidding)
+exports.createBidding = factory.createOne(Bidding)
+exports.getBidding = factory.getOne(Bidding)
+exports.updateBidding = factory.updateOne(Bidding)
+exports.deleteBidding = factory.deleteOne(Bidding)
