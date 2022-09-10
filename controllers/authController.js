c847521e13522e0b40f78ff600af7a1499530c01
NyanPham <phamthanhnhanussh@gmail.com>
2022-09-10 19:20:35 +0700

    checking the login func

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,8 @@ const signToken = (id) =>
 
 const signAndSendToken = (user, res, statusCode) => {
     const token = signToken(user._id)
+    console.log(req.secure)
+    console.log(req.headers['x-forwarded-proto'] === 'https')
     const cookieOptions = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,
